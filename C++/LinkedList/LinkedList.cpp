a4a081d639b523ae96f92919a433e332c478b664
MelihCelik00 <melihsafa.c@gmail.com>
2021-10-17 20:40:47 +0300

    started ds again with linkedlist

--- C++/LinkedList/LinkedList.cpp
+++ C++/LinkedList/LinkedList.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+
+
+struct Node 
+{
+    int data;
+    Node* link;
+}
+
+int main()
+{
+    Node* A;
+    A = NULL;
+
+    Node* temp = new Node();
+
+    temp -> data = 2;
+    temp -> link = NULL;
+
+    A = temp;
+
+    temp = new Node();
+    temp -> data = 4;
+    temp -> link = NULL;
+
+    Node* temp1 = A;
+    while(temp1 -> link != NULL)
+    {
+        temp1= temp1->link;
+    }
+    temp1->link = temp;
+
+}
