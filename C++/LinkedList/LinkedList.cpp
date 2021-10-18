3c292eaafbad64e5bc8851aabf0755710688291e
MelihCelik00 <melihsafa.c@gmail.com>
2021-10-18 22:45:08 +0300

    insertion and deletion implementations are ready

--- C++/LinkedList/LinkedList.cpp
+++ C++/LinkedList/LinkedList.cpp
@@ -8,12 +8,12 @@ class Node
 };
 
 Node* head; // global variable, can be accessed anywhere
-Node* InsertNodeAtBeginning(Node* head,int x)
+Node* InsertNodeAtBeginning(Node* head,int x) // defined return type, trying different implementations
 {
     Node* temp = new Node();
     temp -> data = x;
     temp -> next = NULL;
-    if (head != NULL) temp -> next = head;
+    if (head != NULL) temp -> next = head; // if root is not null, connect head node as new node's link
     head = temp;
     return head;
 }
@@ -23,7 +23,7 @@ void InsertNodeAtNthPosition(int data, int n)
     Node* temp1 = new Node();
     temp1 -> data = data;
     temp1 -> next = NULL;
-    if (n==1) {
+    if (n==1) { // Special Case(1st Position): implementation is like InsertNodeAtBeginning method
         temp1->next = head;
         head = temp1;
         return;
@@ -35,7 +35,38 @@ void InsertNodeAtNthPosition(int data, int n)
     }
     temp1 -> next = temp2 -> next;
     temp2 -> next = temp1;
+}
+
+void InsertNodeAtTheEnd(int data)
+{
+    Node* temp1 = new Node(); // define temporary node as usual
+    temp1 -> data = data;
+    temp1 -> next = NULL;
+    if (head == NULL) head = temp1; // if there is no such linked list
+    if (head != NULL){
+        Node* temp2 = head;
+        while (temp2->next != NULL){
+            temp2 = temp2 -> next;
+        }
+        temp2 -> next = temp1;
+    }
+}
+
+void DeleteNode(int n) // Delete node at position n
+{
+    Node* temp1 = head;
+    if (n==1){ // special case if first node wanted to be deleted
+        head = temp1->next; // head now points the second node
+        free(temp1);
+        return;
+    }
     
+    int i;
+    for (int i = 0; i < n-2; i++) temp1 = temp1->next; //temp1 points the (n-1)th node
+    
+    Node* temp2 = temp1 -> next; // nth node
+    temp1->next = temp2 -> next; // (n+1)th node
+    free(temp2); // delete temp2
 }
 
 void Print()
@@ -52,11 +83,17 @@ void Print()
 
 int main()
 {
-    Node* head = NULL; // local variable, must be passed as referance
+    head = NULL; // if root is defined as local variable, must be passed as referance to the related functions
     InsertNodeAtNthPosition(2, 1); // List: 2
-    InsertNodeAtNthPosition(3, 2); // List: 2, 3
-    InsertNodeAtNthPosition(4, 1); // List: 4, 2, 3
-    InsertNodeAtNthPosition(5, 2); // List: 4, 5, 2, 3
+    InsertNodeAtNthPosition(4, 2); // List: 2, 4
+    InsertNodeAtTheEnd(6);
+    InsertNodeAtTheEnd(5); // List: 2, 4, 6, 5
+    Print();
+    int n;
+    std::cout << "Enter a position: ";
+    std::cin >> n;
+    DeleteNode(n);
+    Print();
     /*std::cout << "\nHow many numbers? " << std::endl;
     int n, x;
     std::cin >> n;
@@ -65,7 +102,5 @@ int main()
         std::cout << "Enter the number: ";
         std::cin >> x;
         head = InsertNodeAtBeginning(head, x);
-    }*/
-    Print();
-    
+    }*/    
 }
