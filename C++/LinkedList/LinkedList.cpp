0bda90c96f47034affd1c33f8f4e3442f6d9ac25
MelihCelik00 <melihsafa.c@gmail.com>
2021-10-18 15:57:27 +0300

    insert node to the nth position

--- C++/LinkedList/LinkedList.cpp
+++ C++/LinkedList/LinkedList.cpp
@@ -18,6 +18,28 @@ Node* InsertNodeAtBeginning(Node* head,int x)
     return head;
 }
 
+Node* InsertNodeAtNthPosition(Node* head, int data, int n)
+{
+    Node* temp1 = new Node();
+    temp1 -> data = data;
+    temp1 -> next = NULL;
+    if (n==1) {
+        temp1->next = head;
+        head = temp1;
+        return head;
+    }
+    Node* temp2 = head;
+    for (int i = 0; i < n-2; i++)
+    {
+        temp2  = temp2 -> next;
+    }
+    temp1 -> next = temp2 -> next;
+    temp2 -> next = temp1;
+    
+    
+    
+}
+
 void Print(Node* head)
 {
     //Node* temp = head; // if printing a global variable, a new declaration must be done here to prevent ruining the global variable
@@ -33,7 +55,11 @@ void Print(Node* head)
 int main()
 {
     Node* head = NULL; // local variable, must be passed as referance
-    std::cout << "\nHow many numbers? " << std::endl;
+    InsertNodeAtNthPosition(head, 2, 1); // List: 2
+    InsertNodeAtNthPosition(head, 3, 2); // List: 2, 3
+    InsertNodeAtNthPosition(head, 4, 1); // List: 4, 2, 3
+    InsertNodeAtNthPosition(head, 5, 2); // List: 4, 5, 2, 3
+    /*std::cout << "\nHow many numbers? " << std::endl;
     int n, x;
     std::cin >> n;
     for (int i = 0; i < n; i++)
@@ -41,7 +67,7 @@ int main()
         std::cout << "Enter the number: ";
         std::cin >> x;
         head = InsertNodeAtBeginning(head, x);
-    }
+    }*/
     Print(head);
     
 }
