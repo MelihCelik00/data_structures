7cf1918813bd7da96cfd87a25c8f5829cad67167
MelihCelik00 <melihsafa.c@gmail.com>
2021-10-25 17:05:20 +0300

    written doubly linked list insertion and print methods

--- C++/DoublyLinkedList/DoublyLinkedList.cpp
+++ C++/DoublyLinkedList/DoublyLinkedList.cpp
@@ -2,7 +2,7 @@
 
 class DoublyLinkedListNode{
     public:
-        int data;
+        int data; // [prev][data][next]
         DoublyLinkedListNode *next;
         DoublyLinkedListNode *prev;
 
@@ -14,23 +14,63 @@ class DoublyLinkedListNode{
 };
 
 DoublyLinkedListNode* GetNewNode(int x){
-    DoublyLinkedListNode newNode = new DoublyLinkedListNode();
+    DoublyLinkedListNode * newNode = new DoublyLinkedListNode();
     newNode->data = x;
-    newNode->prev = NULL;
+    newNode->prev = NULL; 
     newNode->next = NULL;
-    return newNode;
+    return newNode; 
 }
 
-DoublyLinkedListNode* InsertAtHead(DoublyLinkedListNode head, int x){
-    DoublyLinkedListNode* newNode = GetNewNode(x);
+DoublyLinkedListNode* InsertAtHead(DoublyLinkedListNode* head, int x){
+    DoublyLinkedListNode* newNode = GetNewNode(x); 
     if(head == NULL){
         head = newNode;
         return head;
     }
+    head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
+
+void Print(DoublyLinkedListNode* head){
+    DoublyLinkedListNode* temp = head;
+    std::cout << "Print forward: ";
+    while(temp != NULL){
+        std::cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    std::cout << std::endl;
+}
+
+void ReversePrint(DoublyLinkedListNode* head){
+    DoublyLinkedListNode* temp = head;
+    if(temp == NULL) return; // if list is empty, just exit
+    // Going last node
+    while(temp -> next != NULL){
+        temp = temp->next;
+    }
+    // Traversing backward using prev pointer
+    std::cout << "Print backward: ";
+    while(temp != NULL){
+        std::cout << temp->data << " ";
+        temp = temp -> prev;
+    }
+    std::cout << std::endl;
+
 }
 
 int main(){
-    Node* head = NULL; // local variable
+    DoublyLinkedListNode* head = NULL; // local variable
 
+    head = InsertAtHead(head, 2);
+    Print(head);
+    ReversePrint(head);
+    head = InsertAtHead(head, 4);
+    Print(head);
+    ReversePrint(head);
+    head = InsertAtHead(head, 6);
+    Print(head);
+    ReversePrint(head);
 
 }
